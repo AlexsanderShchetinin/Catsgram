855660a98532df0329e923523bcca3254152cb5f
Sportik99 <shchetinin.ase@yandex.ru>
2024-05-01 23:24:36 +0600

    feat: add UserController for API

--- src/main/java/ru/yandex/practicum/catsgram/model/User.java
+++ src/main/java/ru/yandex/practicum/catsgram/model/User.java
@@ -1,11 +1,14 @@
 package ru.yandex.practicum.catsgram.model;
 
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.Instant;
 
-
 @EqualsAndHashCode(of = {"email"})
+@Getter
+@Setter
 public class User {
 
     Long id;    // уникальный идентификатор пользователя
